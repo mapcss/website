0bd13b07e573f12ee6404d25f0623ed8d2381f87
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-03-19 01:55:07 +0900

    feat(hooks): improve resize effect hooks to be switchable

--- hooks/use_resize.ts
+++ hooks/use_resize.ts
@@ -1,21 +1,35 @@
 import { DependencyList, EffectCallback, useEffect, useRef } from "react";
 
+type Option = {
+  /** Whatever enable this effect or not.
+   * @default true
+   */
+  enabled: boolean;
+
+  /** If present, effect will only activate if the values in the list change. */
+  deps: DependencyList;
+};
+
 const useResize = (
-  callback: (ev: UIEvent) => ReturnType<EffectCallback>,
-  deps?: DependencyList,
+  effect: (ev: UIEvent) => ReturnType<EffectCallback>,
+  option: Partial<Option> = { enabled: true },
 ) => {
   const ref = useRef<ReturnType<EffectCallback>>();
-  const fn = (ev: UIEvent) => {
-    ref.current = callback(ev);
-  };
+
   useEffect(() => {
+    if (!option?.enabled) return;
+
+    const fn = (ev: UIEvent) => {
+      ref.current = effect(ev);
+    };
+
     window.addEventListener("resize", fn);
 
     return () => {
       window.removeEventListener("resize", fn);
       ref.current?.();
     };
-  }, deps);
+  }, [option?.enabled, ...option?.deps ?? []]);
 };
 
 export default useResize;
