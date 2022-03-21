8f71531665e6a944966c8b248e91b9aaebd98b83
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-03-22 02:22:56 +0900

    chore: adjust padding

--- utils/message.ts
+++ utils/message.ts
@@ -1,6 +1,12 @@
-export type Message = {
-  type: "error";
-  value: ErrorLike;
-} | { type: "content"; value: string };
+export type Message =
+  | {
+    type: "error";
+    value: ErrorLike;
+  }
+  | { type: "content"; value: string }
+  | {
+    type: "progress";
+    value: "compile" | "import";
+  };
 
 export type ErrorLike = Pick<Error, "message" | "name" | "stack">;
