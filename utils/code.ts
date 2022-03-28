0dd9cf82e2d3791cd9331497bd2c75a627383493
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-03-28 15:39:35 +0900

    chore: update example

--- utils/code.ts
+++ utils/code.ts
@@ -23,7 +23,7 @@ export const CODE =
         <li class="flex items-center">
           <span class="w-6 h-6 flex-none text-teal-500 i-mdi-check-circle">✓</span>
           <p class="ml-4">
-           Link to issue easily
+           Sharing, reporting issue is easy
           </p>
         </li>
       </ul>
@@ -32,7 +32,7 @@ export const CODE =
     <div class="pt-8 text-base leading-7 font-semibold">
       <p class="text-gray-900 dark:text-white">Want to dig deeper into MapCSS?</p>
       <p>
-        <a href="/docs/installation" class="text-amber-500 hover:text-amber-600 transition-colors duration-200">Read the docs<span class="i-mdi-arrow-right-thick">&rarr;</span></a>
+        <a href="/docs/installation" target="_blank" class="text-amber-500 hover:text-amber-600 transition-colors duration-200">Read the docs<span class="i-mdi-arrow-right-thick">&rarr;</span></a>
       </p>
     </div>
   </div>
